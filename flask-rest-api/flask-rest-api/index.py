19d4cfa68147127f8e167dce897f0a6ffb98501d
Nrupesh29 <nrupesh.patel2912@gmail.com>
2020-08-30 10:59:07 -0700

    Added endpoint to fetch student based on id

--- flask-rest-api/flask-rest-api/index.py
+++ flask-rest-api/flask-rest-api/index.py
@@ -2,9 +2,16 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-students = [{"name": "Student 1"}, {"name": "Student 2"}]
+students = [{"id": 1, "name": "Student 1"}, {"id": 2, "name": "Student 2"}]
 
 
-@app.route("/students")
+@app.route("/students/search")
 def get_students():
     return jsonify(students)
+
+
+@app.route("/students/info/<student_id>")
+def get_student(student_id):
+    return jsonify(
+        next(student for student in students if student["id"] == int(student_id))
+    )
